a6502b5a77e26bfd66531a5e88c2eb9ba997e9c3
Christopher DiStasio <cadistasio@gmail.com>
2021-05-20 20:38:09 -0500

    Creates Customer DAO impl, adds getCustomers()

---
+++ src/com/dreamsourcer/springcrmtemplatev2/dao/CustomerImplDAO.java
@@ -0,0 +1,35 @@
+package com.dreamsourcer.springcrmtemplatev2.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dreamsourcer.springcrmtemplatev2.entity.Customer;
+
+@Repository
+public class CustomerImplDAO implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+
+		Session currSess = sessionFactory.getCurrentSession();
+		
+		Query<Customer> theQuery = currSess.createQuery("from customer", Customer.class);
+		
+		List<Customer> allCustomers = theQuery.getResultList();
+	
+		return allCustomers;
+		
+	}
+	
+}
