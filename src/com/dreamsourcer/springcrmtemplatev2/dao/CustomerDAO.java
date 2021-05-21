4e88729106c87a9af8bb55318f2970a5f66fb418
Christopher DiStasio <cadistasio@gmail.com>
2021-05-20 20:37:11 -0500

    Creates Customer DAO interface, adds getCustomers()

---
+++ src/com/dreamsourcer/springcrmtemplatev2/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.dreamsourcer.springcrmtemplatev2.dao;
+
+import java.util.List;
+
+import com.dreamsourcer.springcrmtemplatev2.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+
+}
