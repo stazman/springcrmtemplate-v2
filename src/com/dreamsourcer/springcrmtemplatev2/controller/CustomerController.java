88a882ef50a6fa806c53ab888322627ece98098f
Christopher DiStasio <cadistasio@gmail.com>
2021-05-21 19:56:38 -0500

    Injects customer list dependency into controller

--- src/com/dreamsourcer/springcrmtemplatev2/controller/CustomerController.java
+++ src/com/dreamsourcer/springcrmtemplatev2/controller/CustomerController.java
@@ -1,15 +1,29 @@
 package com.dreamsourcer.springcrmtemplatev2.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dreamsourcer.springcrmtemplatev2.dao.CustomerDAO;
+import com.dreamsourcer.springcrmtemplatev2.entity.Customer;
+
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomers (Model model){
+		
+		List<Customer> customerList = customerDAO.getCustomers();
+		
+		model.addAttribute("customerList", customerList);
 		
 		return "list-customer";
 		
