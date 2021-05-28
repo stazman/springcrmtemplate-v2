7624e679fe837f86f001dc1915f3eae7760728c2
Christopher DiStasio <cadistasio@gmail.com>
2021-05-27 21:51:16 -0500

    Creates, adds getCustomers() to CustomerService interface

---
+++ src/com/dreamsourcer/springcrmtemplatev2/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.dreamsourcer.springcrmtemplatev2.service;
+
+import java.util.List;
+
+import com.dreamsourcer.springcrmtemplatev2.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+
+}
