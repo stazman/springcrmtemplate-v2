8203a28c27f6733512a3240fbaf56c0ec790738e
Christopher DiStasio <cadistasio@gmail.com>
2021-06-17 21:07:48 -0500

    Maps save customer method to controller

--- src/com/dreamsourcer/springcrmtemplatev2/controller/CustomerController.java
+++ src/com/dreamsourcer/springcrmtemplatev2/controller/CustomerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dreamsourcer.springcrmtemplatev2.entity.Customer;
@@ -38,6 +40,15 @@ public class CustomerController {
 		theModel.addAttribute("customer", theCustomer);
 		
 		return "customer-form";
+		
+	}
+	
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return "redirect:/customer/list";
 		
 	}
 	
