a23cb4ffea3c9baad682a69cc9f060c49e106ed7
Christopher DiStasio <cadistasio@gmail.com>
2021-06-29 19:52:30 -0500

    Adds deleteCustomer() to controller; deletes customer from db, view list

--- src/com/dreamsourcer/springcrmtemplatev2/controller/CustomerController.java
+++ src/com/dreamsourcer/springcrmtemplatev2/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,15 @@ public class CustomerController {
 		theModel.addAttribute("customer", theCustomer);
 		
 		return "customer-form";
+	}
+	
+	@GetMapping("/delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId, Model theModel) {
+		
+		customerService.deleteCustomer(theId);
+		
+		return "redirect:/customers/list";
+
 	}
 	
 }
