76a9bc1c4b002c2cb7f9c0aa32fe4091bffbe805
Christopher DiStasio <cadistasio@gmail.com>
2021-07-05 23:58:05 -0500

    Adds, stubs addAccount()

--- src/com/dreamsourcer/springcrmtemplatev2/dao/AccountDAO.java
+++ src/com/dreamsourcer/springcrmtemplatev2/dao/AccountDAO.java
@@ -1,5 +1,10 @@
 package com.dreamsourcer.springcrmtemplatev2.dao;
 
 public class AccountDAO {
+	
+	public void addAccount() {
+		
+		System.out.println(getClass() + "Doing DB Work: Adding an Account");
+	}
 
 }
