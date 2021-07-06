a434672e11230ad936e7399ff29a57c0340cdb6e
Christopher DiStasio <cadistasio@gmail.com>
2021-07-05 23:55:42 -0500

    Creates, stubs AccountConfig

---
+++ src/com/dreamsourcer/springcrmtemplatev2/dao/AccountConfig.java
@@ -0,0 +1,12 @@
+package com.dreamsourcer.springcrmtemplatev2.dao;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.dreamsourcer.springcrmtemplate2.dao")
+public class AccountConfig {
+
+}
