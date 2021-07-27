435d874728f07e1fa1fe46be81919ec39f1a2cdc
Christopher DiStasio <cadistasio@gmail.com>
2021-07-26 22:00:34 -0500

    Creates, stubs main test class

---
+++ src/com/dreamsourcer/springcrmtemplate2/main/AOPMain.java
@@ -0,0 +1,17 @@
+package com.dreamsourcer.springcrmtemplate2.main;
+
+public class AOPMain {
+
+	public static void main(String[] args) {
+
+		// read Spring config java class
+		
+		// get the bean from spring context
+		
+		// call the business method
+		
+		// close the context
+		
+	}
+
+}
