87b2612d2c800348f2603e3ba1a64cf654dc297e
Christopher DiStasio <cadistasio@gmail.com>
2021-07-27 21:55:37 -0500

    Completes AOPMain test class

--- src/com/dreamsourcer/springcrmtemplate2/main/AOPMain.java
+++ src/com/dreamsourcer/springcrmtemplate2/main/AOPMain.java
@@ -1,16 +1,21 @@
 package com.dreamsourcer.springcrmtemplate2.main;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.dreamsourcer.springcrmtemplatev2.dao.AccountConfig;
+import com.dreamsourcer.springcrmtemplatev2.dao.AccountDAO;
+
 public class AOPMain {
 
 	public static void main(String[] args) {
 
-		// read Spring config java class
-		
-		// get the bean from spring context
-		
-		// call the business method
-		
-		// close the context
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AccountConfig.class);
+
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+		theAccountDAO.addAccount();
+
+		context.close();
 		
 	}
 
